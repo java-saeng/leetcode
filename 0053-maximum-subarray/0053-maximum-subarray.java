13eaf73b552e17e4052610d952095667c17401cd
HyeonWoo Kim <62413589+java-saeng@users.noreply.github.com>
2022-10-26 21:57:32 +0900

    Time: 4 ms (11.86%), Space: 82 MB (8.24%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int answer = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            nums[i] = Math.max(nums[i], nums[i] + nums[i - 1]);
+            answer = Math.max(nums[i], answer);
+        }
+
+        return answer;
+    }
+}
